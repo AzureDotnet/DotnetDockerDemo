a60f5c30405da9ffd6f4caa98a4055a84defca7e
AzureDotnet <https://github.com/AzureDotnet>
2025-07-27 01:35:39 +0530

    cicdr

--- obj/Debug/net9.0/DotnetDockerDemo.AssemblyInfo.cs
+++ obj/Debug/net9.0/DotnetDockerDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetDockerDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95e78c6118b217c1edc26bacc021aabb1cdabf51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c38ce86ee1a217c7bfe227a465e8aa76a56801ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetDockerDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetDockerDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
